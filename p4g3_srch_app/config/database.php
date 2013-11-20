9141de3ca64cb35422a99d5e4c773e56a1cbfbf7
Marina Ib <ibrishim@ualberta.ca>
2013-11-19 20:16:17 -0800

    no need for db so set everything to false

--- p4g3_srch_app/config/database.php
+++ p4g3_srch_app/config/database.php
@@ -39,22 +39,22 @@
 */
 
 $active_group = 'default';
-$active_record = TRUE;
-
+$active_record = FALSE;
+ 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
 $db['default']['database'] = '';
-$db['default']['dbdriver'] = '';
+$db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
+$db['default']['pconnect'] = FALSE;
+$db['default']['db_debug'] = FALSE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
+$db['default']['autoinit'] = FALSE;
 $db['default']['stricton'] = FALSE;
 
 /* End of file database.php */
